73e78e956df68c0530e7f568068d37b532267444
n7tms <n7tms@yahoo.com>
2022-12-05 20:45:24 -0700

    2015.06 solved

--- AOC2015/201506.py
+++ AOC2015/201506.py
@@ -2,8 +2,8 @@
 
 import time
 
-# IN_FILE = "AOC2015\\201506.txt"
-IN_FILE = "AOC2015\\201506.sample.txt"
+IN_FILE = "AOC2015\\201506.txt"
+# IN_FILE = "AOC2015\\201506.sample.txt"
 
 
 def get_directions(line):
@@ -29,13 +29,77 @@ def parse():
         for line in f.read().split('\n'):
             out.append(get_directions(line))
     return out
+ 
 
-def part1(data):            # => 238
+def toggle(range_of_lights, lights):
+    xs,ys,xe,ye = map(int,range_of_lights)
+    for x in range (xs,xe+1):
+        for y in range(ys,ye+1):
+            if lights[x][y] == 1:
+                lights[x][y] = 0
+            else: 
+                lights[x][y] = 1
+    return lights
+
+def turn(on_off, range_of_lights, lights):
+    xs,ys,xe,ye = map(int,range_of_lights)
+    for x in range (xs,xe+1):
+        for y in range(ys,ye+1):
+            lights[x][y] = on_off
+    return lights
+
+def part1(data):            # => 543903
     """Solve part 1."""
     lights = [[0] * 1000 for _ in range(1000)]
+    # lights = [[0] * 10 for _ in range(10)]
+    
+    for cmd in data:
+        # print(cmd)
+        if cmd[0] == 'toggle':
+            lights = toggle(cmd[1:],lights)
+        else:
+            lights = turn(1,cmd[1:],lights) if cmd[0] == 'on' else turn(0,cmd[1:],lights)
+    
+    return sum(on.count(1) for on in lights)
 
-def part2(data):            # => 69
+
+def toggle2(range_of_lights, lights):
+    xs,ys,xe,ye = map(int,range_of_lights)
+    for x in range (xs,xe+1):
+        for y in range(ys,ye+1):
+            lights[x][y] += 2
+    return lights
+
+def turn2(on_off, range_of_lights, lights):
+    xs,ys,xe,ye = map(int,range_of_lights)
+    for x in range (xs,xe+1):
+        for y in range(ys,ye+1):
+            if (lights[x][y] > 0 and on_off == -1) or on_off == 1:
+                lights[x][y] += on_off
+    return lights
+
+def total_on(data):
+    total = 0
+    for j in range(len(data)):
+        if type(data[j]) == list :
+            total+= total_on(data[j])
+        else:
+            total += data[j]  
+    return total
+
+def part2(data):            # => 14687245
     """Solve part 2."""
+    lights = [[0] * 1000 for _ in range(1000)]
+    # lights = [[0] * 10 for _ in range(10)]
+    
+    for cmd in data:
+        # print(cmd)
+        if cmd[0] == 'toggle':
+            lights = toggle2(cmd[1:],lights)
+        else:
+            lights = turn2(1,cmd[1:],lights) if cmd[0] == 'on' else turn2(-1,cmd[1:],lights)
+    # print(lights)
+    return total_on(lights)
 
 
 
@@ -43,7 +107,7 @@ if __name__ == "__main__":
     timestart = time.time()
     
     puzzle_input = parse()
-    print(puzzle_input)
+    # print(puzzle_input)
     print("part 1:",part1(puzzle_input))
     print("part 2:",part2(puzzle_input))
     
