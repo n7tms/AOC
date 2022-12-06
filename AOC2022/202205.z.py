2e222b6f12fc8687e29e48bcf53ef4eceae0c821
n7tms <n7tms@yahoo.com>
2022-12-05 19:50:00 -0700

    2022.06 z

--- AOC2022/202205.z.py
+++ AOC2022/202205.z.py
@@ -24,3 +24,32 @@ def getStart():
     return stacks
 
 print(getStart())
+
+
+
+# ==============================================
+# Explore this solution
+# https://www.reddit.com/r/adventofcode/comments/zcxid5/2022_day_5_solutions/iyz0qpi/?context=3
+
+from aocd import data
+from parse import parse
+
+crates, instructions = data.split("\n\n")
+cols0 = {}
+for *row, last in zip(*crates.split("\n")):
+    if last.isdigit():
+        cols0[int(last)] = "".join(x for x in row if x.isupper())
+
+moves = []
+for line in instructions.splitlines():
+    n, p0, p1 = parse("move {:d} from {:d} to {:d}", line).fixed
+    moves.append((n, p0, p1))
+
+for part in "ab":
+    step = -1 if part == "a" else None
+    cols = cols0.copy()
+    for n, p0, p1 in moves:
+        cols[p1] = cols[p0][:n][::step] + cols[p1]
+        cols[p0] = cols[p0][n:]
+    result = "".join([v0 for v0, *_ in cols.values()])
+    print(f"part {part}:", result)
