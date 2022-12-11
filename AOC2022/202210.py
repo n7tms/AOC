a90778f24ed8ccd4ccc6599e6cdbcf0bf0efa335
n7tms <n7tms@yahoo.com>
2022-12-10 19:06:14 -0700

    2022.10 solved

---
+++ AOC2022/202210.py
@@ -0,0 +1,78 @@
+# AOC 2022 - Day 10
+
+import time
+
+# IN_FILE = "AOC2022\\202210.txt"
+IN_FILE = "AOC2022\\202210.sample.txt"
+
+
+def parse():
+    """Parse input."""
+    with open(IN_FILE) as f:
+        # return [[int(c) for c in line.strip()] for line in f]     # integers
+        # return [(line.strip()) for line in f.read().split('\n')]    # strings
+        # return [([dir,mag] for dir,mag in line.split()) for line in f.read().split('\n')]
+        out = [line for line in f.read().split('\n')]
+    return [line.split() for line in out]
+
+
+def part1(data):            # => 12460
+    """Solve part 1."""
+    cycle = 1
+    instridx = 0
+    regval = 1
+    strength = 0
+    inadd = False
+    crt = ""
+
+    while True:
+        if instridx >= len(data) or cycle > 220:
+            break
+
+        instr = data[instridx]
+
+        if regval == cycle - 1 or regval == cycle or regval == cycle +1:
+            crt += '#'
+        else:
+            crt += '.'
+
+
+        if cycle == 20 or (cycle > 59 and (cycle - 20) % 40 == 0):
+            strength += cycle * regval
+            # print(cycle,strength)
+        if inadd:
+            inadd = False
+            regval += int(instr[1])
+            instridx += 1
+        elif instr[0] == 'noop':
+            instridx += 1
+        else:
+            if instr[0] == 'addx':
+                inadd = True
+
+        cycle += 1
+
+
+    return strength,crt
+
+def part2(data):            # => EZFPRAKL
+    """Solve part 2."""
+
+
+if __name__ == "__main__":
+    timestart = time.time()
+    tm = [[0] * 600] * 600  # initialize tail map
+
+    puzzle_input = parse()
+    # print(puzzle_input)
+
+    part1_solution,crt = part1(puzzle_input)
+    print("part 1:",part1_solution)
+    # print("part 2:",part2(puzzle_input))
+    print("part 2:")
+    for i in range(6):
+        print(crt[i:i+40])
+    
+    timeend = time.time()
+    print("Execution time: ", "{:.7f}".format(round(timeend-timestart,7)))
+
---
+++ AOC2022/202210.z.py
@@ -0,0 +1,24 @@
+# AOC 2022 - day 10
+# adapted from 'juanplopes'
+
+IN_FILE = "AOC2022\\202210.txt"
+
+def parse():
+    """Parse input."""
+    with open(IN_FILE) as f:
+        return [line for line in f.read().split('\n')]
+
+def execute(program):
+    regval = 1
+    for line in program:
+        yield regval
+        if line[0] == 'addx':
+            yield regval
+            regval += int(line[1])
+
+out = list(execute(line.split() for line in parse()))
+print(sum(out[i - 1] * i for i in [20,60,100,140,180,220]))
+
+# print the CRT
+for i in range(6):
+    print(''.join('.#'[abs(out[i * 40 + j] - j) <= 1] for j in range(40)))
