0ed0c2ed6c5c90ded81fef65c0eb4268ad57e587
n7tms <n7tms@yahoo.com>
2022-12-19 00:11:42 -0700

    2022.18 solved

--- AOC2022/202218.py
+++ AOC2022/202218.py
@@ -20,8 +20,7 @@ def parse():
 def part1(data):            # => 3374
     """Solve part 1."""
     not_connected = 0
-    for cube in data:
-        x,y,z = cube
+    for x,y,z in data:
         if [x+1,y,z] not in data:
             not_connected += 1
         if [x,y+1,z] not in data:
@@ -36,12 +35,47 @@ def part1(data):            # => 3374
             not_connected += 1
     return not_connected
             
-    
+def is_exposed(data,x,y,z):
+    not_connected = 0
+    for x,y,z in data:
+        if [x+1,y,z] not in data:
+            not_connected += 1
+        if [x,y+1,z] not in data:
+            not_connected += 1
+        if [x,y,z+1] not in data:
+            not_connected += 1
+        if [x-1,y,z] not in data:
+            not_connected += 1
+        if [x,y-1,z] not in data:
+            not_connected += 1
+        if [x,y,z-1] not in data:
+            not_connected += 1
+    return not_connected
 
 
-
-def part2(data):            # => 1562536022966
+def part2(data):            # => 2010
     """Solve part 2."""
+    xs,ys,zs,xm,ym,zm = 2,2,2,2,2,2
+    for x,y,z in data:
+        xs = max([xs,x])
+        ys = max([ys,y])
+        zs = max([zs,z])
+        xm = min([xm,x])
+        ym = min([ym,y])
+        zm = min([zm,z])
+    print(xm,ym,zm,xs,ys,zs)
+    exposed = 0
+    for x,y,z in data:
+        if x == xs or x == xm or y == ys or y == ym or z == zm or z == zs:
+            exposed += is_exposed(data,x,y,z)
+    return exposed
+        
+
+
+
+
+
+
 
 if __name__ == "__main__":
     timestart = time.time()
