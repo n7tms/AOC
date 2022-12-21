e8b42cbd149432f404f772b42adfa2ff5f8ef994
n7tms@yahoo.com <n7tms@yahoo.com>
2022-12-21 15:28:40 -0700

    2022.21 solved

--- AOC2022/202221.py
+++ AOC2022/202221.py
@@ -2,8 +2,8 @@
 
 import time
 
-IN_FILE = "AOC2022\\inputs\\202221.txt"
-# IN_FILE = "AOC2022\\inputs\\202221.sample.txt"
+IN_FILE = "AOC2022/inputs/202221.txt"
+# IN_FILE = "AOC2022/inputs/202221.sample.txt"
 
 
 def parse():
@@ -38,14 +38,48 @@ def yelling(data, monkey):
 
 
 def part1(data):            # => 364367103397416
-    """Solve part 1."""
     return yelling(data,'root')
         
 
+def part2(data):            # => 3782852515583
 
-def part2(data):            # => 
-    """Solve part 2."""
+    # This is a TOTAL hack!
+    # After some brute force failures (literally about a million of them),
+    # I pasted the results of the first 20 brute-force trials into a spreadsheet to look for patterns.
+    # I found that the difference between trial(n) and trial(n-1) was a constant...
+    # Dividing the difference between both sides of the 'root' operation and that constant
+    # gives me a number -- a number that just happened to be the correct answer!
+    #
+    # I don't know if this will work with other inputs, but it worked with mine.
+
+    # The constant is 43.75
+
+    # "data" is a dictionary; 
+    # key = monkey name, value = equation or literal associated with that monkey
+
+    # Get both sides of the 'root' monkey
+    m1,_,m2 = data['root'].split(' ')
+
+    # We need two trials to get the difference numbers. It doesn't matter which trials we select,
+    # as long as they're consecutive. I selected 0 and 1.
     data['humn'] = 0
+
+    # Get answers for both sides of 'root' in trial 0
+    t01 = yelling(data,m1)
+    t02 = yelling(data,m2)
+    # and calculate the difference. 
+    diff = t01-t02
+
+    # We only need an answer for the left side of the 'root' monkey in trial 1
+    data['humn'] = 1
+    t11 = yelling(data,m1)
+    # and calculate the difference between the first trial (0) and this one (1).
+    # This is the constant we're look for.
+    the_constant = t01 - t11
+
+    # The answer is the previous difference (diff) divided by the constant
+    ans = diff/the_constant
+    return ans
     
 
 if __name__ == "__main__":
