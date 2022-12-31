5ae8eea012c204a0b0abac3e8ed3a5aa9b78734a
n7tms <n7tms@yahoo.com>
2022-12-30 17:21:51 -0700

    2017.08 solved

--- AOC2017/08.py
+++ AOC2017/08.py
@@ -1,35 +1,52 @@
-# AOC 2017 - Day 07
-# tags: #graph #regex
+# AOC 2017 - Day 08
+# tags: 
 
 import time
 import re
 
-IN_File = "AOC2017/07.z.txt"
+IN_File = "AOC2017/08.txt"
+registers = {}
 
 def parse():
     """Parse input."""
     with open(IN_File) as f:
         out = f.read().split('\n')
+    return out
+
+def part1(data):    # part1: 7296; part2: 8186
+    max_memory = 0
+    for line in data:
+        instr = line.split(' ',4)
+        reg,ud,mag,_,cond = instr
+        if reg not in registers:
+            registers[reg] = 0
+
+        condition = cond.split(' ')
+        reg2,op,val = condition
+        if reg2 not in registers:
+            registers[reg2] = 0
+        cnd = str(registers[reg2]) + op + val
+        if eval(cnd):
+            if ud == 'inc':
+                registers[reg] += int(mag)
+            else:
+                registers[reg] -= int(mag)
+        max_memory = max(max_memory,registers[reg])
+
+    return registers[max(registers, key=registers.get)],max_memory
 
 
-
-def part1(data):    # 
-    steps = 0
-    return steps
-
-def part2(data):    # 
-    steps = 0
-    return steps
 
 if __name__ == "__main__":
     timestart = time.time()
 
-    data1 = parse()
+    data = parse()
     # print(data)
-    data2 = data1.copy()
 
-    print("part 1:",part1(data1))
-    print("part 2:",part2(data2))
+    maxreg,maxmem = part1(data)
+
+    print("part 1:",maxreg)
+    print("part 2:",maxmem)
     
     timeend = time.time()
     print("Execution time: ", "{:.4f}".format(round(timeend-timestart,7)))
