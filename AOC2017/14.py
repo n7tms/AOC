c3ef42abed5420d0521c9a80e6aa5e7b3d69dda2
n7tms@yahoo.com <n7tms@yahoo.com>
2023-01-04 15:14:49 -0700

    2017.14 in progress

---
+++ AOC2017/14.py
@@ -0,0 +1,33 @@
+# AOC 2017 - Day 14
+# tags: 
+
+import time
+
+IN_File = "AOC2017/14.txt"
+
+
+def parse():
+    with open(IN_File) as f:
+        out = f.read().split('\n')
+    
+    
+    return
+
+
+def part1():    # 
+    return 
+
+def part2():    # 
+    return
+
+
+if __name__ == "__main__":
+    timestart = time.time()
+
+    parse()
+
+    print("part 1:",part1())
+    print("part 2:",part2())
+
+    timeend = time.time()
+    print("Execution time: ", "{:.4f}".format(round(timeend-timestart,7)))
