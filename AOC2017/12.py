540aec1fc6af6c8e76daa2a413894e55eeddfcdc
n7tms@yahoo.com <n7tms@yahoo.com>
2023-01-04 11:18:25 -0700

    2017.12 solution

--- AOC2017/12.py
+++ AOC2017/12.py
@@ -1,23 +1,56 @@
 # AOC 2017 - Day 12
-# tags: 
+# tags: #sets
 
 import time
+from collections import defaultdict
 
 IN_File = "AOC2017/12.txt"
 
+edges = set()
+node_map = defaultdict(set)
 
 def parse():
     with open(IN_File) as f:
-        out = f.read().split(',')
+        out = f.read().split('\n')
+    
+    for line in out:
+        nodes = line.split('<->')
+        for node in nodes[1].split(','):
+            edges.add((int(nodes[0]),int(node)))
 
-    return out
+    return
 
+zero_connects = []
 
-def part1(data):    # 
-    return 
+def count_conns(node):
+    for x in node:
+        if x in zero_connects:
+            continue
+        else:
+            zero_connects.append(x)
+            count_conns(node_map[x])
+    return
 
-def part2(data):    # 
-    return 
+def part1(data):    # 115
+
+    for x,y in edges:
+        node_map[x].add(y)
+        node_map[y].add(x)
+
+    count_conns(node_map[0])
+
+    return len(zero_connects)
+
+def part2(data):    # 221
+    groups = 1
+
+    for n in range(1,2000):
+        if n in zero_connects:
+            continue
+        else:
+            groups += 1
+            count_conns(node_map[n])
+    return groups
 
 
 
