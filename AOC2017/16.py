cdc12fd217b4d5aa37b938b2d79af5678ecf9ed4
n7tms <n7tms@yahoo.com>
2023-01-05 20:20:31 -0700

    2017.16 solution

--- AOC2017/16.py
+++ AOC2017/16.py
@@ -1,11 +1,12 @@
 # AOC 2017 - Day 16
-# tags: 
+# tags: #large #memoize #repeating
 
 import time
 
 IN_File = "AOC2017/16.txt"
 prgms = 'abcdefghijklmnop'
-# prgms = 'abcde'
+
+billion = 1000000000
 
 def parse():
     with open(IN_File) as f:
@@ -39,62 +40,22 @@ def part1(moves,prgms):    # namdgkbhifpceloj
     return prgms
 
 def part2(moves,prgms):    # 
-    # the following block will take forever -- >5000 hours
-    # for _ in range(1000000000):
-    #     part1(moves,prgms)
-    # return prgms
-
-    # so, just follow the pattern:
-    # a moves to 1
-    # b moves to 6
-    # c moves to 11
-    # etc. 
-    # do those moves 1000000000 times
-    new_moves = []
-    new_moves.append([0,prgms.index('a')]) # a
-    new_moves.append([1,prgms.index('b')]) # b
-    new_moves.append([2,prgms.index('c')]) # c
-    new_moves.append([3,prgms.index('d')]) # d
-    new_moves.append([4,prgms.index('e')]) # e
-    new_moves.append([5,prgms.index('f')]) # f
-    new_moves.append([6,prgms.index('g')]) # g
-    new_moves.append([7,prgms.index('h')]) # h
-    new_moves.append([8,prgms.index('i')]) # i
-    new_moves.append([9,prgms.index('j')]) # j
-    new_moves.append([10,prgms.index('k')]) # k
-    new_moves.append([11,prgms.index('l')]) # l
-    new_moves.append([12,prgms.index('m')]) # m
-    new_moves.append([13,prgms.index('n')]) # n
-    new_moves.append([14,prgms.index('o')]) # o
-    new_moves.append([15,prgms.index('p')]) # p
-
-    pl = []
-    for c in prgms:
-        pl.append(c)
-
-    history = []
-    for i in range(1000000000):
-        tmp = []
-        for c in  new_moves:
-            tmp.append(pl[c[0]])
-
-        for c in new_moves:
-            pl[c[1]] = tmp[c[0]]
-        
-        if pl not in history:
-            history.append([pl])
+    history = ['abcdefghijklmnop']
+    for i in range(billion):
+        prgms = part1(moves,prgms)
+        if prgms in history:
+            break   # quit if we start to see a duplicate
         else:
-            return i,pl
-    
-    return ''.join(pl)
-    
+            history.append(prgms)
+
+    index = billion % (i + 2)
+    return history[index-1]
 
 
 if __name__ == "__main__":
     timestart = time.time()
 
     data = parse()
-    # data = ['s1','x3/4','pe/b']
 
     prgms = part1(data,prgms)
     print("part 1:",prgms)
