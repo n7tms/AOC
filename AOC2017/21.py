a39438fe224ccdddedc6003ff380906cff0526f7
n7tms <n7tms@yahoo.com>
2023-01-08 20:45:05 -0700

    2017.21 in progress

---
+++ AOC2017/21.py
@@ -0,0 +1,28 @@
+# AOC 2017 - Day 21
+# tags: #
+
+import time
+
+IN_File = "AOC2017/21.txt"
+
+def parse():
+    with open(IN_File) as f:
+        out = f.read().split('\n')
+
+
+def part1(data):    # 
+    pass
+
+def part2(data):    # 
+    pass
+
+if __name__ == "__main__":
+    timestart = time.time()
+
+    data = parse()
+
+    print("part 1:",part1(data))
+    print("part 2:",part2(data))
+
+    timeend = time.time()
+    print("Execution time: ", "{:.4f}".format(round(timeend-timestart,7)))
