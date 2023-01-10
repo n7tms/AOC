f42c992909a259da5af66c14741607192216e08b
n7tms <n7tms@yahoo.com>
2023-01-10 12:38:58 -0700

    2019.04 solution

--- AOC2019/04.py
+++ AOC2019/04.py
@@ -56,6 +56,53 @@ def part2(data):    # 607
             count += 1
     return count
 
+def other_solution():
+    # This solution is from @sanjithpk and I do not understand for sure
+    # how it works. 
+    # I've tried to add comments for some sort of explanation.
+
+    import collections
+
+    low = 271973
+    high = 785961
+    a1 = []
+    a2 = []
+
+    t = low
+
+    while True:
+        d = list(str(t))
+        
+        # If the i+1 digit is less than the i digit, then
+        # this number (t) doesn't work. 
+        # Then fill the rest of the digits with the i+1 digit
+        # and break out of the for loop.
+        for i in range(5):
+            if d[i] > d[i+1]:
+                d[i + 1:6] = d[i] * (5-i)
+                t = int("".join(d))
+                d = list(str(t))
+                break
+        
+        # If we're past the upper limit, break out of the while.
+        if t > high:
+            break
+
+        # (This is where things get iffy.)
+        # I think this is checking for at least a double digit
+        if(collections.Counter(str(t)).most_common(1)[0][1] > 1):
+            # if true, add this pwd to the part1 group.
+            a1.append(t)
+            
+        # I think this is checking for a least two double digits.
+        if(2 in collections.Counter(str(t)).values()):
+            # if true, add this pwd to the part2 group.
+            a2.append(t)
+        t += 1
+        
+    print(len(a1), len(a2))
+
+
 if __name__ == "__main__":
     timestart = time.time()
 
