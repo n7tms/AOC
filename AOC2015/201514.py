cc321d64becbcd7ed70f59f02bc6167eb084313c
n7tms <n7tms@yahoo.com>
2023-03-03 21:10:06 -0700

    2015.14 solution

--- AOC2015/201514.py
+++ AOC2015/201514.py
@@ -3,8 +3,8 @@
 import time
 from collections import defaultdict
 
-IN_FILE = "AOC2015\\201514.txt"
-# IN_FILE = "AOC2015\\201514.sample.txt"
+# IN_FILE = "AOC2015\\201514.txt"
+IN_FILE = "AOC2015\\201514.sample.txt"
 
  
 
@@ -15,37 +15,33 @@ def parse():
     reindeers = defaultdict(dict)
     for line in out:
         name, speed, duration, rest = line.split(' ')[0], int(line.split(' ')[3]), int(line.split(' ')[6]), int(line.split(' ')[13])
-        reindeers[name]={"speed":speed, "duration":duration, "rest":rest, "resting":False, "rested": 0, "ran": duration, "distance": 0}
+        reindeers[name]={"speed":speed, "duration":duration, "rest":rest, "resting":False, "time": duration, "distance": 0, "score": 0}
     return reindeers
 
 
 def one_second(reindeers):
     for reindeer in reindeers:
         # print(reindeer)
-        if reindeers[reindeer]["resting"]:
-            if reindeers[reindeer]["rested"] == 0:
-                reindeers[reindeer]["resting"] = False
-                reindeers[reindeer]["ran"] = reindeers[reindeer]["duration"]
-                # reindeers[reindeer]["distance"] += reindeers[reindeer]["speed"]
-            else:
-                reindeers[reindeer]["rested"] -= 1
+        if reindeers[reindeer]["resting"] and reindeers[reindeer]["time"] == 0:
+            reindeers[reindeer]["resting"] = False
+            reindeers[reindeer]["time"] = reindeers[reindeer]["duration"]
+        elif not reindeers[reindeer]["resting"] and reindeers[reindeer]["time"] == 0:
+            reindeers[reindeer]["resting"] = True
+            reindeers[reindeer]["time"] = reindeers[reindeer]["rest"]
+        elif not reindeers[reindeer]["resting"]:
+            reindeers[reindeer]["distance"] += reindeers[reindeer]["speed"]
+            reindeers[reindeer]["time"] -= 1
         else:
-            if reindeers[reindeer]["ran"] == 0:
-                reindeers[reindeer]["resting"] = True
-                reindeers[reindeer]["rested"] = reindeers[reindeer]["rest"]
-            else:
-                reindeers[reindeer]["distance"] += reindeers[reindeer]["speed"]
-                reindeers[reindeer]["ran"] -= 1
+            reindeers[reindeer]["time"] -= 1
+
     return reindeers
 
 
 def part1(reindeers):          # -> 2640
     race = one_second(reindeers)
     for i in range(2502):
-    # for i in range(999):
-        # if i == 136:
-        #     t=3
         race = one_second(race)
+    print(reindeers)
     
     winner = [None,0]
     for r in race:
@@ -54,19 +50,38 @@ def part1(reindeers):          # -> 2640
     return winner
 
 
+def part2(reindeers):          # -> 1102
+    # for i in range(2503):
+    for i in range(1000):
+        reindeers = one_second(reindeers)
 
-def part2(reindeers):          # ->  >1064
-    pass
+        winning = [None,0]
+        for r in reindeers:
+            if reindeers[r]["distance"] > winning[1]:
+                winning = [r,reindeers[r]["distance"]]
+    
+        for r in reindeers:
+            if reindeers[r]["distance"] == winning[1]:
+                reindeers[r]["score"] += 1
+
+    print(reindeers)
+    
+    winner = [None,0]
+    for r in reindeers:
+        if reindeers[r]["score"] > winner[1]:
+            winner = [r,reindeers[r]["score"]]
+    return winner
 
 if __name__ == "__main__":
     timestart = time.time()
     
     reindeers = parse()
+    reindeers2 = parse()
     # print(reindeers)
 
 
     print("part 1:",part1(reindeers))
-    print("part 2:",part2(reindeers))
+    print("part 2:",part2(reindeers2))
     
     timeend = time.time()
     print("Execution time: ", "{:.7f}".format(round(timeend-timestart,7)))
---
+++ AOC2015/201514.y.py
@@ -0,0 +1,19 @@
+# @fatpollo
+
+import re
+import itertools
+import collections
+
+text = open('AOC2015\\201514.txt').read()
+regex = r'(\w+) can fly (\d+) km/s for (\d+) seconds, but then must rest for (\d+) seconds.'
+history = collections.defaultdict(list)
+for who, speed, duration, rest in re.findall(regex, text):
+    steps = itertools.cycle([int(speed)]*int(duration) + [0]*int(rest))
+    history[who] = list(itertools.accumulate(next(steps) for _ in range(2503)))
+
+by_dist = max(h[-1] for h in history.values())
+print(by_dist)
+
+scored = [i for a in zip(*history.values()) for i, v in enumerate(a) if v==max(a)]
+by_points = max(collections.Counter(scored).values())
+print(by_points)
