b9c7a8483bb89baad1fa24714325cc5f73f01ec4
n7tms@yahoo.com <n7tms@yahoo.com>
2023-03-06 15:55:17 -0700

    2015.18 in progress

--- AOC2015/201518.py
+++ AOC2015/201518.py
@@ -2,17 +2,63 @@
 
 import time
 
-IN_FILE = "AOC2015/201517.txt"
+# IN_FILE = "AOC2015/201518.txt"
+IN_FILE = "AOC2015/201518.sample.txt"
 
 def parse():
     with open(IN_FILE) as f:
         out = [line for line in f.read().split('\n')]
 
-    out = list(map(int,out))
+    out = list(list(x) for x in out)
     return out
 
+
+def adj_ons(grid,row,col):
+    adjacents = [[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]]
+
+    new_coords = []
+    for x in adjacents:
+        new_coords.append([x[0]+row,x[1]+col])
+
+    on_count = 0
+    for r,c in new_coords:
+        if r < 0 or c < 0 or r > len(grid)-1 or c > len(grid[0])-1:
+            pass
+        else:
+            if grid[r][c] == '#':
+                on_count += 1
+    
+    return on_count
+
+
 def part1(data):    # => 
-    return 0
+    working = list(data)
+    for r in range(len(data)):
+        for c in range(len(data[0])):
+            # how many adjacent lights?
+            adj_on = adj_ons(data,r,c)
+            if data[r][c] == '#' and (adj_on == 2 or adj_on == 3):
+                # leave light on
+                pass
+            elif data[r][c] == '.' and adj_on == 3:
+                # turn light on
+                pass
+            else:
+                # toggle the light
+                if data[r][c] == '#':
+                    working[r][c] == '.'
+                else:
+                    working[r][c] == '#'
+    data = list(working)
+    print(working)
+
+    final_on_count = 0
+    for r in range(len(data)):
+        for c in range(len(data[0])):
+            if data[r][c] == '#':
+                final_on_count += 1
+
+    return final_on_count
 
 def part2(data):    # => 
     return 0
