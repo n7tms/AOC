8f2079a49839680da9a65cf21408ec07caffe3c3
n7tms@yahoo.com <n7tms@yahoo.com>
2023-03-07 15:03:06 -0700

    2015.18 solution

--- AOC2015/201518.py
+++ AOC2015/201518.py
@@ -1,9 +1,17 @@
 # --- Day 18:  ---
 
+# This is VERY sloppy and slow. I could not get a copy of the initial list without creating a reference.
+# Even using list.copy(), it still created a reference.
+# I ended up creating a new list to keep track of which lights needed to change.
+# Then iterating through that list and toggling the lights in the original list that need to change.
+# The result is O(3n^2) -- which lights need to change; change the lights; count the on lights.
+# ...and do that twice -- part 1 and part 2. The resulting execution time is bout 24 seconds. TOO LONG!
+
+
 import time
 
-# IN_FILE = "AOC2015/201518.txt"
-IN_FILE = "AOC2015/201518.sample.txt"
+IN_FILE = "AOC2015/201518.txt"
+# IN_FILE = "AOC2015/201518.sample.txt"
 
 def parse():
     with open(IN_FILE) as f:
@@ -31,47 +39,91 @@ def adj_ons(grid,row,col):
     return on_count
 
 
-def part1(data):    # => 
-    working = list(data)
-    for r in range(len(data)):
-        for c in range(len(data[0])):
-            # how many adjacent lights?
-            adj_on = adj_ons(data,r,c)
-            if data[r][c] == '#' and (adj_on == 2 or adj_on == 3):
-                # leave light on
-                pass
-            elif data[r][c] == '.' and adj_on == 3:
-                # turn light on
-                pass
-            else:
-                # toggle the light
-                if data[r][c] == '#':
-                    working[r][c] == '.'
-                else:
-                    working[r][c] == '#'
-    data = list(working)
-    print(working)
+def part1(data,cycles):    # => 821
+    gs = len(data) # grid size
+
+    for _ in range(cycles):
+        working = [[0 for i in range(gs)] for j in range(gs)]
+        for r in range(gs):
+            for c in range(gs):
+                # how many adjacent lights?
+                adj_on = adj_ons(data,r,c)
+                if data[r][c] == '#' and not (adj_on == 2 or adj_on == 3):
+                    # turn light off
+                    working[r][c] = 1
+                elif data[r][c] == '.' and adj_on == 3:
+                    # turn light on
+                    working[r][c] = 1
+    
+        # toggle the lights that should change
+        for r in range(gs):
+            for c in range(gs):
+                if working[r][c] == 1:
+                    if data[r][c] == '#':
+                        data[r][c] = '.'
+                    else:
+                        data[r][c] = '#'
 
     final_on_count = 0
-    for r in range(len(data)):
-        for c in range(len(data[0])):
+    for r in range(gs):
+        for c in range(gs):
             if data[r][c] == '#':
                 final_on_count += 1
 
     return final_on_count
 
-def part2(data):    # => 
-    return 0
+def part2(data,cycles):    # => 886
+    gs = len(data) # grid size
+    # turn on the four corners
+    data[0][0] = '#'
+    data[0][gs-1] = '#'
+    data[gs-1][0] = '#'
+    data[gs-1][gs-1] = '#'
+
+    for _ in range(cycles):
+        working = [[0 for i in range(gs)] for j in range(gs)]
+        for r in range(gs):
+            for c in range(gs):
+                if (r == 0 and c == 0) or (r == gs-1 and c == gs-1) or (r == 0 and c == gs-1) or (r == gs-1 and c == 0):
+                    # ignore the corner lights
+                    pass
+                else:
+                    # how many adjacent lights?
+                    adj_on = adj_ons(data,r,c)
+                    if data[r][c] == '#' and not (adj_on == 2 or adj_on == 3):
+                        # turn light off
+                        working[r][c] = 1
+                    elif data[r][c] == '.' and adj_on == 3:
+                        # turn light on
+                        working[r][c] = 1
+    
+        # toggle the lights that should change
+        for r in range(gs):
+            for c in range(gs):
+                if working[r][c] == 1:
+                    if data[r][c] == '#':
+                        data[r][c] = '.'
+                    else:
+                        data[r][c] = '#'
+
+    final_on_count = 0
+    for r in range(gs):
+        for c in range(gs):
+            if data[r][c] == '#':
+                final_on_count += 1
+
+    return final_on_count
 
 
 if __name__ == "__main__":
     timestart = time.time()
 
     puzzle_input = parse()
+    puzzle_input2 = parse()
 
     print("\nDay 18: ===========================")
-    print("part 1:",part1(puzzle_input))
-    print("part 2:",part2(puzzle_input))
+    print("part 1:",part1(puzzle_input,100))
+    print("part 2:",part2(puzzle_input2,100))
     
     timeend = time.time()
     print("Execution time: ", f"{timeend-timestart:0.4f}", "\n")
