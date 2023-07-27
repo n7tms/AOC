22d6c813f2d715fd2a1c3cd959c0cdc9a32a14f8
n7tms@yahoo.com <n7tms@yahoo.com>
2023-07-27 17:07:47 -0600

    2019.10 in progress

--- AOC2019/10.py
+++ AOC2019/10.py
@@ -43,3 +43,18 @@ sample = [
 # ....7
 # ...87
 
+
+# This code might come in handy
+import numpy as np
+arr = np.array([[1,2,3,4],[5,6,7,8]])
+for idx, x in np.ndenumerate(arr):
+    print(idx,x)
+# outputs
+#  (0,0) 1
+#  (0,1) 2
+# etc
+# consider NetworkX package
+
+
+
+
