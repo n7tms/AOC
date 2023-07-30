cc943b075af6e243f3fd60467461a6be1779f9d6
n7tms <n7tms@yahoo.com>
2023-07-30 14:36:30 -0600

    2019.11 in progress

--- AOC2019/11.py
+++ AOC2019/11.py
@@ -1,14 +1,21 @@
 # Advent of Code 2019: day 11
 import getaoc as ga
+import intcode_c as ic
+
+
 
 def get_input():
     data = ga.get_input(11,2019).splitlines()
+    data = [int(line) for line in data[0].split(',')]
     return data
 
 
 
-def part1():
-    ...
+def part1(program):
+    code = ic.Intcode('Paint Robot',program,[1])
+    print(code.run())
+
+    return None
 
 
 
@@ -16,7 +23,8 @@ def part2() -> None:
     ...
 
 def main():
-    part1()
+    program = get_input()
+    part1(program)
     part2()
 
 if __name__ == "__main__":
