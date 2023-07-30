976ba1a644fb5dbf658d17bfab4087c39e22cb0d
n7tms <n7tms@yahoo.com>
2023-07-30 14:27:13 -0600

    2019.11 in progress

---
+++ AOC2019/11.py
@@ -0,0 +1,23 @@
+# Advent of Code 2019: day 11
+import getaoc as ga
+
+def get_input():
+    data = ga.get_input(11,2019).splitlines()
+    return data
+
+
+
+def part1():
+    ...
+
+
+
+def part2() -> None:
+    ...
+
+def main():
+    part1()
+    part2()
+
+if __name__ == "__main__":
+    main()
