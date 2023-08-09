1ba935e53e2c83b0cf218f69dddb304f4d4777bf
n7tms@yahoo.com <n7tms@yahoo.com>
2023-08-09 10:43:36 -0600

    2019.12 solution

--- AOC2019/12b.py
+++ AOC2019/12b.py
@@ -6,6 +6,8 @@ import logging
 import getaoc as ga
 import intcode_c as ic
 import re
+from itertools import combinations
+import time
 
 def get_input():
     """ Using the get_input from my getaoc module, read the input from the web
@@ -16,19 +18,16 @@ def get_input():
 
         <x=7, y=10, z=17>
     """
-    moons = list()
     data = ga.get_input(12,2019).splitlines()
     
     for line in data:
         match = re.search(r'<x=(-*\d+), y=(-*\d+), z=(-*\d+)', line)
         logging.info(f"Matched Moon: {match.group(1)} {match.group(2)} {match.group(3)}")
-        # print(match.group(1), match.group(2), match.group(3))
-        m = Moon(match.group(1), match.group(2), match.group(3))
-        moons.append(m)
-
-    return moons
+        m = Moon(int(match.group(1)), int(match.group(2)), int(match.group(3)))
+    return Moon.get_moons()
 
 class Moon:
+    moons = []
 
     def __init__(self,px, py, pz):
         self.posx = px
@@ -38,7 +37,16 @@ class Moon:
         self.vely = 0
         self.velz = 0
         self.states = list()
+        Moon.moons.append(self)
 
+    @classmethod
+    def get_moons(cls):
+        return list(Moon.moons)
+    
+    @classmethod
+    def clear_moons(cls):
+        Moon.moons.clear()
+    
     def compare(self, other_moon):
         """ Compare 'this' moon to the other moon.
             Returns the other moon."""
@@ -70,8 +78,8 @@ class Moon:
         self.posy += self.vely
         self.posz += self.velz
 
-    def save_state(self):
-        self.states.append(self.state)
+    # def save_state(self):
+    #     self.states.append(self.state)
 
     def potential_energy(self) -> int:
         """sum of abs of postions"""
@@ -88,102 +96,87 @@ class Moon:
     def state(self) -> str:
         return f"{self.posx},{self.posy},{self.posz} ({self.velx},{self.vely},{self.velz})"
     
-    def previous_state(self) -> bool:
-        return True if self.state in self.states else False
-            
-
+    # def previous_state(self) -> bool:
+    #     return True if self.state in self.states else False
+           
     def __str__(self):
         return f"{self.posx},{self.posy},{self.posz} ({self.velx},{self.vely},{self.velz})"
 
 
-def compare_moons(moon1, moon2) -> list:
+def part1(moons):        # -> 9958
     """"""
-
-def apply_velocities(moon) -> dict:
-    """"""
-
-def part1():        # -> 9958
-    """"""
-    m1 = Moon(7,10,17)
-    m2 = Moon(-2,7,0)
-    m3 = Moon(12,5,12)
-    m4 = Moon(5,-8,6)
 
     for _ in range(1000):
-        m2 = m1.compare(m2)
-        m3 = m1.compare(m3)
-        m4 = m1.compare(m4)
-        m3 = m2.compare(m3)
-        m4 = m2.compare(m4)
-        m4 = m3.compare(m4)
+        # compare pairs of moons to each other and update velocities
+        for pair in combinations(moons,2):
+            m1,m2 = pair
+            m2 = m1.compare(m2)
 
-        m1.update_positions()
-        m2.update_positions()
-        m3.update_positions()
-        m4.update_positions()
+        # based on new velocities, update each moon's position
+        for m in moons:
+            m.update_positions()
 
-    print(m1)
-    print(m2)
-    print(m3)
-    print(m4)
-
-    total_energy = m1.total_energy() + m2.total_energy() + m3.total_energy() + m4.total_energy()
+    total_energy = sum([m.total_energy() for m in moons])
     return total_energy
     
     
 
-def part2():
+def part2(moons):   # -> 318382803780324
     """"""
-    m1 = Moon(7,10,17)
-    m2 = Moon(-2,7,0)
-    m3 = Moon(12,5,12)
-    m4 = Moon(5,-8,6)
+
+    states = list()
+
+    # save the initial state
+    state = list()
+    for m in moons:
+        state.append(f"{m}")
+    states.append(state)
 
     steps = 0
     while True:
-        m2 = m1.compare(m2)
-        m3 = m1.compare(m3)
-        m4 = m1.compare(m4)
-        m3 = m2.compare(m3)
-        m4 = m2.compare(m4)
-        m4 = m3.compare(m4)
+        # compare pairs of moons to each other and update velocities
+        for pair in combinations(moons,2):
+            m1,m2 = pair
+            m2 = m1.compare(m2)
 
-        m1.update_positions()
-        m2.update_positions()
-        m3.update_positions()
-        m4.update_positions()
+        # based on new velocities, update each moon's position
+        for m in moons:
+            m.update_positions()
 
-        if m1.previous_state():
-            break
-
-        m1.save_state()
-        m2.save_state()
-        m3.save_state()
-        m4.save_state()
+        # save the current state
+        state = list()
+        for m in moons:
+            state.append(f"{m}")
         
-        steps += 1
+        if state in states:
+            return steps
+        else:
+            states.append(state)       
+            steps += 1
 
-    return steps
 
 
 def main():
     """solve each part and print the solutions"""
 
     # Instantiate the logger
-    log_level = logging.DEBUG
+    # log_level = logging.DEBUG
     # log_level = logging.INFO
-    # log_level = logging.WARNING
+    log_level = logging.WARNING
     log_format = '%(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=log_level, format=log_format)
 
-    program1 = get_input()
-    # program2 = get_input()
+    moons = get_input()
+    print(f"Total Energy: {part1(moons)}")
 
-    # part1(program1)
-    # part2(program2)
+    # start over for part2
+    Moon.clear_moons()
+    moons = get_input()
 
-    print(f"Total Energy: {part1()}")
-    print(f"Steps: {part2()}")
+    st = time.time()
+    print(f"Steps: {part2(moons)}")
+    et = time.time()
+    print(f"Elapsed time: {et - st} seconds")
 
 if __name__ == "__main__":
     main()
