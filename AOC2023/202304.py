c2f0626ac6ac31146c3eb094cd65d4c7d9219f9f
n7tms@yahoo.com <n7tms@yahoo.com>
2023-12-04 11:04:12 -0700

    2023.04 solved

--- AOC2023/202304.py
+++ AOC2023/202304.py
@@ -4,199 +4,86 @@
 import aoc_utils as aoc
 import numpy as np
 
-IN_FILE = "AOC2023\\inputs\\202304.txt"
-# IN_FILE = "AOC2023\\inputs\\202304.sample.in"
+# IN_FILE = "AOC2023\\inputs\\202304.in"
+# IN_FILE = "AOC2023\\inputs\\202304.sample.txt"
 
-# IN_FILE = "./inputs/202304.txt"
-# IN_FILE = "./inputs/202304.sample.in"
-
-
-
+IN_FILE = "AOC2023/inputs/202304.in"
+# IN_FILE = "AOC2023/inputs/202304.sample.txt"
 
 def parse(puzzle_input):
     """
-    Part Parse
+    Parse
+
+        Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
     """
     with open(IN_FILE) as fp:
         out = []
         data = fp.read().splitlines()
+
     return data
 
 
-def get_number(data,row,col) -> int:
-    in_number = True
-    the_number = []
-    while in_number and col < len(data[row]):
-        if data[row][col].isdigit():
-            the_number.append(data[row][col])
-            col += 1
-        else:
-            in_number = False
-    return int("".join(the_number))
-
-def adjacent_symbol(data,row,col) -> bool:
-    """ returns False if no symbol is adjacent; otherwise True."""
-    for d in aoc.DIRS:
-        r = row + d[0]
-        c = col + d[1]
-
-        if r < 0 or c < 0 or c >= len(data[row]) or r >= len(data):
-            pass
-        else:
-            if (not data[r][c].isdigit()) and (data[r][c] != "."):
-                return True
-    return False
-
-def get_whole_number(data,row,col) -> int:
-    number = []
-
-    # look left
-    c = col
-    while data[row][c].isdigit() and c >= 0:
-        number.insert(0,data[row][c])
-        c -= 1
-    
-    # look right
-    c = col + 1
-    while c < len(data[row]) and data[row][c].isdigit():
-        number.append(data[row][c])
-        c += 1
-    
-    return int("".join(number))
-
-
-def adjacent_part_number_upl(data,row,col) -> int:
-    DIRS = ([-1,-1],[0,-1],[-1,0],[-1,1])
-    for d in DIRS:
-        r = row + d[0]
-        c = col + d[1]
-        if data[r][c].isdigit():
-            return get_whole_number(data,r,c)
-    
-    return 0
-
-
-def adjacent_part_number_dnr(data,row,col) -> int:
-    DIRS = ([1,-1],[1,0],[0,1],[1,1])
-    for d in DIRS:
-        r = row + d[0]
-        c = col + d[1]
-        if data[r][c].isdigit():
-            return get_whole_number(data,r,c)
-    
-    return 0
-
-def adjacent_part_number(data,row,col) -> int:
-    pn1 = 0
-    pn2 = 0
-
-    # look up
-    DIRS = ([-1,-1],[-1,0],[-1,1])
-    for d in DIRS:
-        r = row + d[0]
-        c = col + d[1]
-        if data[r][c].isdigit():
-            if pn1:
-                pn2 = get_whole_number(data,r,c)
-            else:
-                pn1 = get_whole_number(data,r,c)
-    
-    # look left
-    DIRS = ([0,-1],)
-    for d in DIRS:
-        r = row + d[0]
-        c = col + d[1]
-        if data[r][c].isdigit():
-            if pn1:
-                pn2 = get_whole_number(data,r,c)
-            else:
-                pn1 = get_whole_number(data,r,c)
-
-    # look right
-    DIRS = ([0,1],)
-    for d in DIRS:
-        r = row + d[0]
-        c = col + d[1]
-        if data[r][c].isdigit():
-            if pn1:
-                pn2 = get_whole_number(data,r,c)
-            else:
-                pn1 = get_whole_number(data,r,c)
-
-    # look down
-    DIRS = ([1,-1],[1,0],[1,1])
-    for d in DIRS:
-        r = row + d[0]
-        c = col + d[1]
-        if data[r][c].isdigit():
-            if pn1:
-                pn2 = get_whole_number(data,r,c)
-            else:
-                pn1 = get_whole_number(data,r,c)
-
-    print(f"found: {pn1} and {pn2}")
-    return pn1 * pn2
-
-
-
-
-def part1(data):            # => 521601
+def part1(data):            # => 23941
     """
     Solve part 1
+
+    Points for matching cards....
     """
+    cards = []
+    total_points = 0
+    for card in data:
+        _,line = card.split(":")
+        winners,mine = line.split("|")
 
-    in_number = False
-    already_added = False
-    sum_of_part_numbers = 0
-    part_number = 0
+        # add the number to the list if it isn't ""
+        w = [x for x in winners.split(" ") if x]
+        m = [x for x in mine.split(" ") if x]
 
-    for r in range(len(data)):
-        for c in range(len(data[r])):
-            if data[r][c].isdigit():
-                if not in_number: 
-                    # figure out what this number is
-                    part_number = get_number(data,r,c)
-                    in_number = True
+        # points for each of my numbers (m) in the winning numbers (w)
+        # points double for each matching point ... 2^points
+        points = sum(x in m for x in w)
+        if points > 0:
+            total_points += np.power(2,points-1)
 
-                if already_added: 
-                    continue
-                elif adjacent_symbol(data,r,c):
-                    sum_of_part_numbers += part_number
-                    already_added = True
+    return total_points
 
-            else:
-                already_added = False
-                in_number = False
-                part_number = 0
-
-# figure out what the number is
-# iterate across the ndigits of the number looking for adjacent symbols
-# if a symmbol is found, return the number
-#                 
-
-
-        
-    
-    return sum_of_part_numbers
-
-
-def part2(data):            # => >69,616,591   <81,685,336
+def part2(data):            # => 5571760
     """
     Solve part 2
     
+    Now, everything is made up and the points don't matter!
+    Win additional cards based on matches.
     """
-    sum_gear_ratios = 0
-    for r in range(len(data)):
-        for c in range(len(data[r])):
-            if data[r][c] == "*":
-                sum_gear_ratios += adjacent_part_number(data,r,c)
-                # pn1 = adjacent_part_number_upl(data,r,c)
-                # if pn1:
-                #     pn2 = adjacent_part_number_dnr(data,r,c)
-                # print(f"found: {pn1} and {pn2}")
-                # sum_gear_ratios += (pn1 * pn2)
+    count_of_cards = {}
 
-    return sum_gear_ratios
+    total_points = 0
+    for card in data:
+        c,line = card.split(":")
+
+        # get the card number and add it to the dictionary (count_of_cards), if it isn't already there.
+        _,s_card_num = c.strip().split("Card ")
+        i_card_num = int(s_card_num.strip())
+        if not i_card_num in count_of_cards:
+            count_of_cards[i_card_num] = 1
+        else:
+            count_of_cards[i_card_num] += 1
+
+        
+        # get the number of matching numbers
+        winners,mine = line.split("|")
+        w = [x for x in winners.split(" ") if x]
+        m = [x for x in mine.split(" ") if x]
+        matches = sum(x in m for x in w)
+
+        # increment card numbers for matching cards
+        for cn in range(i_card_num+1,i_card_num+matches+1):
+            if not cn in count_of_cards:
+                count_of_cards[cn] = count_of_cards[i_card_num]
+            else:
+                count_of_cards[cn] += count_of_cards[i_card_num]
+
+        # count the total number of cards and return
+    return sum(count_of_cards.values())
 
 
 def solve(puzzle_input):
