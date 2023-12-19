3d0a028299f774b40d5d47cbd172b7578a822e63
n7tms@yahoo.com <n7tms@yahoo.com>
2023-12-19 11:54:10 -0700

    2023.19 p1 done.

--- AOC2023/202319.py
+++ AOC2023/202319.py
@@ -1,10 +1,11 @@
-# AOC 2023 day 19: 
+# AOC 2023 day 19: Aplenty
 #
 
 import aoc_utils as aoc
 import time
 import os
 import numpy as np
+import re
 
 DAY = 19
 IN_FILE = os.path.join("AOC2023","inputs","2023"+str(DAY)+".in")
@@ -17,27 +18,77 @@ def parse(puzzle_input):
     aoc.get_input(2023,DAY,False)
 
     with open(IN_FILE) as fp:
-        data = fp.read().strip().split("\n")
+        data = fp.read().strip().split("\n\n")
     
-    return data
+    a,b = data[0].split('\n'), data[1].split('\n')
+
+    workflows = {}
+    for workflow in a:
+        match = re.match(r"(\w+){(.+)}", workflow)
+        if match:
+            name, actions = match.groups()
+            workflows[name] = actions.split(',')
+
+    parts = []
+    for item in b:
+        matches = re.match(r'{x=(\d+),m=(\d+),a=(\d+),s=(\d+)}',item)
+        if matches:
+            parts.append({
+                'x': int(matches.group(1)),
+                'm': int(matches.group(2)),
+                'a': int(matches.group(3)),
+                's': int(matches.group(4))
+            })
+
+    return workflows, parts
 
 
 
 
-def part1(data):        # => 
+def part1(data):        # => 487623
     """
     Solve part 1
     
     """
+    workflows, parts = data
 
-    return
+    accepted = []
+
+    for part in parts:
+        pc = 'in'
+        # branch = ''
+        while not (pc == 'A' or pc == 'R'):
+            wfs = workflows[pc]
+            for wf in wfs:
+                if wf == 'A' or wf == 'R': 
+                    pc = wf
+                    break
+                if ':' in wf:
+                    exp,branch = wf.split(':')
+                    m = part[exp[0]]
+                    n = exp[1:]
+                    o = str(str(m)+n)
+                    g = eval(str(str(part[exp[0]])+exp[1:]))
+                    if g:
+                        pc = branch
+                        break
+                else:
+                    pc = wf
+                    break
+        
+        if pc == 'A':
+            accepted.append(sum(part.values()))
+
+    return sum(accepted)
 
 
 def part2(data):            # => 
     """
     Solve part 2
     """
-
+    print(4000*(4000-537)*2439*(4000-3449)*1415*(4000-2663)*(4000-839)*(4000-2006)*(4000-2091)*1716)
+    print(4000*4000*4000*4000)
+    print(167409079868000)
 
     return
 
