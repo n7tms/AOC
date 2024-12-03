47918feba3dde9c53f3a98f4e4d8171c4c1365e8
n7tms@yahoo.com <n7tms@yahoo.com>
2024-12-03 13:04:56 -0700

    2018 03 optimized

--- AOC2018/03.py
+++ AOC2018/03.py
@@ -10,6 +10,21 @@ DAY = '03'
 IN_FILE = os.path.join("AOC2018","inputs","2018-"+str(DAY)+".in")
 # IN_FILE = os.path.join("AOC2018","inputs","2018-"+str(DAY)+".sample.txt")
 
+
+def assemble_set(claim):
+    # claim_set = set()
+    # for r in range(claim['height']):
+    #     for c in range(claim['width']):
+    #         claim_set.add((claim['row']+r,claim['col']+c))
+    # return claim_set
+
+    # this set comprehension is a bit faster and much more pythonic!
+    return {
+        (claim['row'] + r, claim['col'] + c)
+        for r in range(claim['height'])
+        for c in range(claim['width'])
+    }
+
 def parse(puzzle_input):
     """
     Parse
@@ -24,36 +39,43 @@ def parse(puzzle_input):
         matches = re.findall(r'\d+', line)
         claim, col, row, width, height = matches
         claims[claim] = {'row':int(row), 'col':int(col), 'width':int(width), 'height':int(height)}
+        claims[claim]['claim_set'] = set(assemble_set(claims[claim]))
 
     return claims
 
 
-def assemble_set(claim):
-    claim_set = set()
-    for r in range(claim['height']):
-        for c in range(claim['width']):
-            claim_set.add((claim['row']+r,claim['col']+c))
-    return claim_set
-
 def part1(claims):        # => 115348
-
     fabric = set()
     overlap = set()
     for claim in claims:
-        claim_set = assemble_set(claims[claim])
+        claim_set = claims[claim]['claim_set']
         if  claim_set & fabric:
-            overlap.update(claim_set & fabric)
+            overlap.update(claims[claim]['claim_set'] & fabric)
         fabric.update(claim_set)
 
     return len(overlap)
 
 
 
+def part2(claims):       # => Claim # 188
+    # add claims set to the claims
+    # iterate through all the claims
+    # if a claim does not intersect with any other claims -- you found it!
 
-def part2(data):       # => 
+    # for claim in claims:
+    #     claims[claim]["claim_set"] = assemble_set(claims[claim])
+    
+    # for k in claims:
+    #     unique = True
+    #     for k2 in claims:
+    #         if k != k2:
+    #             if claims[k]['claim_set'] & claims[k2]['claim_set']:
+    #                 unique = False
+    #     if unique: return k
 
-    return 0
-
+    for k, v in claims.items():
+        if all(v['claim_set'].isdisjoint(claims[k2]['claim_set']) for k2 in claims if k != k2):
+            return k
 
 def solve(puzzle_input):
     """Solve the puzzle for the given input."""
