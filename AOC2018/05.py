105fa621dd48a2d9457c49195c7c026d31854fad
n7tms@yahoo.com <n7tms@yahoo.com>
2024-12-04 13:45:52 -0700

    2018 05 optimized

--- AOC2018/05.py
+++ AOC2018/05.py
@@ -4,6 +4,7 @@
 import aoc_utils as aoc
 import time
 import os
+# import string
 
 
 DAY = '05'
@@ -25,22 +26,35 @@ def parse(puzzle_input):
 def opposites(p1,p2):
     return abs(ord(p1) - ord(p2)) == 32
 
-def part1(polymer):        # => 9296
 
+def reduce_polymer(p):
     idx = 0
-    while idx < len(polymer)-1:
-        if not opposites(polymer[idx], polymer[idx+1]):
+    while idx < len(p)-1:
+        if not opposites(p[idx], p[idx+1]):
             idx += 1
         else:
-            polymer = polymer[:idx] + polymer[idx+2:]
+            p = p[:idx] + p[idx+2:]
             if idx > 0:
                 idx -= 1
 
-    return len(polymer)     
+    return len(p)     
 
-def part2(data):       # => 
 
-    return
+def part1(polymer):        # => 9296
+    return reduce_polymer(polymer)
+
+def part2(data):           # => 5534
+    uLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    lLetters = 'abcdefghijklmnopqrstuvwxyz'
+    
+    reaction_size = len(data)
+
+    # Create a set of (uppercase, lowercase) pairs
+    letter_pairs = zip(uLetters,lLetters)
+
+    # Iterate through each letter pair and compute the minimum reaction size
+    reaction_size = min(reduce_polymer(data.translate(str.maketrans('', '', uc + lc))) for uc, lc in letter_pairs)
+    return reaction_size
 
 def solve(puzzle_input):
     """Solve the puzzle for the given input."""
