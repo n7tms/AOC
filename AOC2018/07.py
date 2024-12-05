bef26eb94c584320fadd2b88da4a293ef020d8f1
n7tms@yahoo.com <n7tms@yahoo.com>
2024-12-05 15:42:47 -0700

    2018 07 p1 & p2 solved

--- AOC2018/07.py
+++ AOC2018/07.py
@@ -4,7 +4,7 @@
 import aoc_utils as aoc
 import time
 import os
-import re
+# import re
 import networkx as nx
 
 
@@ -21,12 +21,10 @@ def parse(puzzle_input):
     with open(IN_FILE) as fp:
         data = fp.read().strip().splitlines()
     
-    instructions = []
-    for line in data:
-        # instr = list(re.match(r"Step (.+) must be finished before step (.+) can begin.", line).groups())
-        instr = re.match(r"Step (.+) must be finished before step (.+) can begin.", line).groups()
-        instructions.append(instr)
-        
+    # instructions = [re.match(r"Step (.+) must be finished before step (.+) can begin.", line).groups() for line in data]
+
+    # because each job is only one character, we can get a way with this simplier method and avoid regex
+    instructions = [(line[5], line[36]) for line in data]
     return instructions
 
 
@@ -46,7 +44,7 @@ def fix(rules, pages):
 
 
 def part1(data):        # => EBICGKQOVMYZJAWRDPXFSUTNLH
-    # the networkx module has a cool routine does does this exact thing!!
+    # the networkx module has a cool routine that does this exact thing!!
     graph = nx.DiGraph()
     graph.add_edges_from(data)
     order = list(nx.lexicographical_topological_sort(graph))
@@ -54,8 +52,48 @@ def part1(data):        # => EBICGKQOVMYZJAWRDPXFSUTNLH
     return ''.join(order)
 
 
-def part2(data):        # => 
-    return 
+def part2(data, job_order):        # => 906
+    # put the jobs in a "queue"
+    # keep track of seconds (timer) (each job takes 60 + ord(job_letter))
+    # if workers < 5, pop a job out of the queue onto workers
+    # at each tick of the timer, decrement the duration on the worker jobs
+    # if a job is complete (duration=0), put it in completed
+    # see if the next job can start (if it's dependency are completed[])
+
+
+    jobs = [j for j in job_order]
+    workers = {}        # {job: duration}
+    completed = []
+    timer = 0
+
+    while True:
+
+        # search for completed jobs
+        for k in list(workers.keys()):
+            workers[k] -= 1
+            if workers[k] == 0:
+                completed.append(k)
+                del workers[k]
+
+        # add next job
+        if len(workers) < 5:
+            for job in list(jobs):
+                # get a list of dependencies
+                dp = [f for f,s in data if s==job]
+
+                # see if all of the dependencies are already completed
+                if all(e in completed for e in dp):
+                    duration = ord(job) - ord("A") + 1 + 60
+                    workers[job] = duration
+                    jobs.remove(job)
+                    if len(workers) >= 5: break
+        
+        # if there are no more workers, then we're done.
+        if not workers:
+            return timer
+        else:
+            timer += 1
+
 
 def solve(puzzle_input):
     """Solve the puzzle for the given input."""
@@ -67,7 +105,7 @@ def solve(puzzle_input):
     print(f"part 1: {p1} ({exec_time:.4f} sec)")
 
     start_time = time.time()
-    p2 = str(part2(data))
+    p2 = str(part2(data,p1))
     exec_time = time.time() - start_time
     print(f"part 2: {p2} ({exec_time:.4f} sec)")
 
