e1967bd882ce2b5225ffb58bdf7a456e20934663
n7tms@yahoo.com <n7tms@yahoo.com>
2024-12-10 11:28:50 -0700

    2018 12 solved

--- AOC2018/12.py
+++ AOC2018/12.py
@@ -19,74 +19,71 @@ def parse(puzzle_input):
     with open(IN_FILE) as fp:
         data = fp.read().strip().split('\n\n')
     
-    initial_state = [c for c in data[0].split(': ')[1]]
+    is0 = [c for c in data[0].split(': ')[1]]
+    # initial_state = []
+    # for inum, ival in enumerate(is0):
+    #     if ival == '#':
+    #         initial_state.append[inum]
+
+    initial_state = [inum for inum,ival in enumerate(is0) if ival=='#']
+
     ins_temp = data[1].splitlines()
-    instructions = []
+    instructions = {}
     for inst in ins_temp:
         pattern, result = inst.split(' => ')
-        instructions.append([pattern, result])
+        instructions[pattern] = result
 
     return initial_state, instructions
 
 
-#########################################################
-# Consider storing just the locations where plants exist
-#########################################################
+# I got this idea (but not the code) from reddit.
+# store the pot numbers, rather than a visual representation of the empty and full pots -- just the pots with plants.
+# using the pattern list comprehension, construct the hash and period pattern looks like around that number.
+# compare that pattern to the instruction dictionary.
+# if the instruction is present, add the result (from the dictionary) to a temporary list containing the new plants/structure.
+# worked like a charm!!!
 
-def part1(instructions, initial_state):        # => >1309, not 2939, not 1340
-    generations = 20
-    initial_len = len(initial_state)
-    final_state = initial_state.copy()
-    pot = 0
-
-
-    print(f'0: {''.join(final_state)} (len: {len(final_state)})')
-    for gen in range(1,generations+1):
-        final_state.insert(0,'.')
-        final_state.insert(0,'.')
-        # final_state.insert(0,'.')
-        # final_state.append('.')
-        final_state.append('.')
-        final_state.append('.')
-        int_state = []
-
-        for pot in range(len(final_state)-2):
-            changed = False
-            plants = ''.join(final_state[pot:pot+5])
-            for i, r in instructions:
-                if i == plants:
-                    int_state.append(r)
-                    changed = True
-                    break
-            if not changed: int_state.append(final_state[pot+2])
-
-        final_state = int_state.copy()
-        print(f'{gen}: {''.join(final_state)} (len: {len(final_state)})')
-
-    final_len = len(final_state)
-    # diff_len = (final_len - initial_len)//3
-    diff_len = (final_len - initial_len)//2
+def part1(instructions, initial_state, generations):        # => 3221   (>1309, not 2939, not 1340)
     final_sum = 0
-    for idx, p in enumerate(final_state):
-        if p == '#':
-            final_sum += (idx-diff_len)
-            
+    
+    final_state = initial_state.copy()
+    for gen in range(generations):
+        prev_sum = final_sum
+        temp_state = []
+
+        cur_plant = min(final_state) - 2
+        max_plant = max(final_state) + 2
+        while cur_plant < max_plant:
+            pattern = ''.join(['#' if cur_plant + offset in final_state else '.' for offset in range(-2,3)])
+            if pattern in instructions and instructions[pattern] == '#':
+                temp_state.append(cur_plant)
+            cur_plant += 1
+        final_state = temp_state.copy()
+
+        final_sum = sum(final_state)    
+        # print(f'{gen}: {sum(final_state)}  (diff {final_sum-prev_sum})')
+
     return final_sum
 
 
-def part2(instructions, initial_state):        # => 
-    return
+def part2(instructions, initial_state):        # => 2600000001872
+    # a little bit of manual intervention here.
+    # at 129 generations, the sum increases by 52 in EVERY generation.
+    # So, (50000000000 - 129) * 52 = 2599999993292
+    # that plus the 8580 that was the sum so far gives us 2600000001872
+    return 2600000001872
 
 def solve(puzzle_input):
     """Solve the puzzle for the given input."""
     initstat,instr = parse(puzzle_input)
 
     start_time = time.time()
-    p1 = str(part1(instr, initstat))
+    p1 = str(part1(instr, initstat, 20))
     exec_time = time.time() - start_time
     print(f"part 1: {p1} ({exec_time:.4f} sec)")
 
     start_time = time.time()
+    # p2 = str(part1(instr, initstat, 50000000000))
     p2 = str(part2(instr, initstat))
     exec_time = time.time() - start_time
     print(f"part 2: {p2} ({exec_time:.4f} sec)")
