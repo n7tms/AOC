97054c534f1add5ead40bd35c7dd0645a4f8cf7c
n7tms@yahoo.com <n7tms@yahoo.com>
2024-12-11 13:26:39 -0700

    2018 16 prepped

---
+++ AOC2018/16.py
@@ -0,0 +1,51 @@
+# AOC 2018 day 15: 
+#
+
+import aoc_utils as aoc
+import time
+import os
+
+
+DAY = '15'
+IN_FILE = os.path.join("AOC2018","inputs","2018-"+str(DAY)+".in")
+# IN_FILE = os.path.join("AOC2018","inputs","2018-"+str(DAY)+".sample.txt")
+
+def parse():
+    """
+    Parse
+    """
+    aoc.get_input(2018,DAY,False)
+
+    with open(IN_FILE) as fp:
+        data = fp.read().splitlines()
+    
+    return 
+
+
+
+def part1(data):        # => 
+    return
+
+def part2(data):        # =>  
+    return
+
+
+
+def solve():
+    """Solve the puzzle for the given input."""
+    data = parse()
+
+    start_time = time.time()
+    p1 = str(part1(data))
+    exec_time = time.time() - start_time
+    print(f"part 1: {p1} ({exec_time:.4f} sec)")
+
+    start_time = time.time()
+    p2 = str(part2(data))
+    exec_time = time.time() - start_time
+    print(f"part 2: {p2} ({exec_time:.4f} sec)")
+
+
+if __name__ == "__main__":
+    solve()
+
