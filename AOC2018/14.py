49b19eaf185d68d0ddf6312c53819bcf1542549b
n7tms@yahoo.com <n7tms@yahoo.com>
2024-12-10 17:22:51 -0700

    2018 14 in progress

--- AOC2018/14.py
+++ AOC2018/14.py
@@ -4,10 +4,11 @@
 import aoc_utils as aoc
 import time
 import os
+from collections import deque
 
 
 DAY = '14'
-IN_FILE = os.path.join("AOC2018","inputs","2018-"+str(DAY)+".in")
+# IN_FILE = os.path.join("AOC2018","inputs","2018-"+str(DAY)+".in")
 # IN_FILE = os.path.join("AOC2018","inputs","2018-"+str(DAY)+".sample.txt")
 
 def parse():
@@ -24,6 +25,21 @@ def parse():
 
 
 def part1(data):        # => 
+    recipes = [3,7]
+    elf1 = 0
+    elf2 = 1
+
+    round = 0
+    while round < data:
+        new_recipe = recipes[elf1] + recipes[elf2]
+        if new_recipe > 9:
+            recipes.append(1)
+            recipes.append(new_recipe-10)
+        else:
+            recipes.append(new_recipe)
+        elf1 = (recipes[elf1] + 1) % len(recipes) + elf1
+        elf2 = (recipes[elf2] + 1) % len(recipes) + elf2
+
 
 
     return 
@@ -34,7 +50,8 @@ def part2():        # =>
 
 def solve():
     """Solve the puzzle for the given input."""
-    data = parse()
+    # data = parse()
+    data = 765071
 
     start_time = time.time()
     p1 = str(part1(data))
