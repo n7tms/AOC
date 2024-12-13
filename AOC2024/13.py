cd3db24b4978a2ebd98dd1a11414f9e6ecd6b075
n7tms@yahoo.com <n7tms@yahoo.com>
2024-12-13 08:45:05 -0700

    2024 13 solved

--- AOC2024/13.py
+++ AOC2024/13.py
@@ -10,32 +10,56 @@ import aoc_utils as aoc
 import time
 import os
 from collections import defaultdict
+import sympy as sp
+import re
 
 DAY = '13'
-# IN_FILE = os.path.join("AOC2024","inputs","2024-"+str(DAY)+".in")
-IN_FILE = os.path.join("AOC2024","inputs","2024-"+str(DAY)+".sample.txt")
+IN_FILE = os.path.join("AOC2024","inputs","2024-"+str(DAY)+".in")
+# IN_FILE = os.path.join("AOC2024","inputs","2024-"+str(DAY)+".sample.txt")
 
 
 aoc.get_input(2024,DAY,False)
 with open(IN_FILE) as fp:
-    data = fp.read().strip().splitlines()
+    data = fp.read().strip().split('\n\n')
+
+r = r'X[+=](-?\d+), Y[+=](-?\d+)'
+games = []
+for groups in data:
+    matches = re.findall(r,groups)
+    games.append([[int(x), int(y)] for x,y in matches])
 
 
-DIRS = [(-1,0), (0,1), (1,0), (0,-1)]
 
 start_time = time.time()
-# Solution for Part 1 here vvvvvvvvvvvvvvvvvvvvvvvvv
+# Solution for Part 1 here vvvvvvvvvvvvvvvvvvvvvvvvv     # => 29438
+tokens = 0
+offset = 10000000000000
+for game in games:
+    A = sp.Matrix([[game[0][0],game[1][0]],[game[0][1],game[1][1]]])
+    b = sp.Matrix([game[2][0],game[2][1]])
+    x = A.solve(b)
+    # print(x[0])
+    if int(x[0]) == x[0] and int(x[1]) == x[1]:
+        tokens += (x[0] * 3) + x[1]
 
-
-part1 = 0
+part1 = tokens
 print(f'Part 1: {part1}')
 
 
 
-# Solution for Part 2 here vvvvvvvvvvvvvvvvvvvvvvvvv
+# Solution for Part 2 here vvvvvvvvvvvvvvvvvvvvvvvvv    # => 104958599303720
+tokens = 0
+offset = 10000000000000
+for game in games:
+    A = sp.Matrix([[game[0][0],game[1][0]],[game[0][1],game[1][1]]])
+    b = sp.Matrix([game[2][0]+offset,game[2][1]+offset])
+    x = A.solve(b)
+    # print(x[0])
+    if int(x[0]) == x[0] and int(x[1]) == x[1]:
+        tokens += (x[0] * 3) + x[1]
 
 
-part2 = 0
+part2 = tokens
 print(f'Part 2: {part2}')
 
 
