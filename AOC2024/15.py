99c75be37669447058ad1191bc587fdb44e0e9ef
Todd Smith <n7tms@yahoo.com>
2024-12-15 10:12:26 -0700

    2024 15 in progress

--- AOC2024/15.py
+++ AOC2024/15.py
@@ -6,8 +6,8 @@ import time
 import os
 
 DAY = '15'
-IN_FILE = os.path.join("AOC2024","inputs","2024-"+str(DAY)+".in")
-# IN_FILE = os.path.join("AOC2024","inputs","2024-"+str(DAY)+".sample.txt")
+# IN_FILE = os.path.join("AOC2024","inputs","2024-"+str(DAY)+".in")
+IN_FILE = os.path.join("AOC2024","inputs","2024-"+str(DAY)+".sample.txt")
 
 def parse(puzzle_input):
     """
@@ -16,13 +16,57 @@ def parse(puzzle_input):
     aoc.get_input(2024,DAY,False)
 
     with open(IN_FILE) as fp:
-        data = fp.read().strip().splitlines()
+        data = fp.read().strip().split('\n\n')
+    
+    walls = set()
+    robots = set()
+    map = []
+    for r, line in enumerate(data[0].splitlines()):
+        row = []
+        for c, ch in enumerate(line):
+            if ch == '#': walls.add((r,c))
+            if ch == 'O': robots.add((r,c))
+            if ch == '@': start = (r,c)
+            row.append(ch)
+        map.append(row)
+    
+    moves = [ch for ch in data[1] if ch != '\n']
 
-    return data
+    return map, walls, robots, moves, start
+
+DIRS = {'^':(-1,0), '>':(0,1), 'v':(1,0), '<':(0,-1)}
+
+
+def part1(map,walls,robots,moves,start):        # => 
+
+    pos = start
+    while moves:
+        mv = moves.pop(0)
+        dir = DIRS[mv]
+        nr, nc = pos[0]+dir[0], pos[1]+dir[1]
+        if map[nr][nc] == '.':
+            pos = (nr,nc)
+        elif (nr,nc) in walls:
+            pass
+        elif (nr,nc) in robots:
+            # iterate in the direction. if find a wall, pass. if find a '.', move everything.
+            shifts = []
+            while True:
+                mr += dir[0]
+                mc += dir[1]
+                if (mr,mc) in walls: break
+                shifts.append((mr,mc))
+                if map[mr][mc] == '.':
+                    for sh in shifts:
+                        robots.add(sh)
+                    del robots[(nr,nc)]
+                    pos = (nr,nc)
+
+
+                    
 
 
 
-def part1(data):        # => 
     return
 
 
@@ -31,10 +75,10 @@ def part2(data):       # =>
 
 def solve(puzzle_input):
     """Solve the puzzle for the given input."""
-    data = parse(puzzle_input)
+    m,w,r,mv,s = parse(puzzle_input)
 
     start_time = time.time()
-    p1 = str(part1(data))
+    p1 = str(part1(m,w,r,mv,s))
     exec_time = time.time() - start_time
     print(f"part 1: {p1} ({exec_time:.4f} sec)")
 
