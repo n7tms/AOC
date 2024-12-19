4856761c42bd57f986e88bb0e2400bfab647b839
n7tms@yahoo.com <n7tms@yahoo.com>
2024-12-19 08:45:23 -0700

    2024 18 solved

--- AOC2024/18.py
+++ AOC2024/18.py
@@ -4,12 +4,10 @@
 import aoc_utils as aoc
 import time
 import os
-import re
 
 DAY = '18'
 IN_FILE = os.path.join("AOC2024","inputs","2024-"+str(DAY)+".in")
 # IN_FILE = os.path.join("AOC2024","inputs","2024-"+str(DAY)+".sample.txt")
-# IN_FILE = os.path.join("AOC2024","inputs","2024-"+str(DAY)+".sampleb.txt")
 
 def parse(puzzle_input):
     """
@@ -20,41 +18,74 @@ def parse(puzzle_input):
     with open(IN_FILE) as fp:
         data = fp.read().strip().splitlines()
     
-    falling_bytes = [[int(x),int(y)] for x,y in [for line in data.split(',')]]
-
-
+    falling_bytes = [list(map(int,line.split(','))) for line in data]
     return falling_bytes
 
 
 
-def part1(pgm, regs):        # => 4,1,5,3,1,5,3,5,7
+def part1(falling_bytes):        # => 438
     """
     Solve part 1
     
     """
+    width = 71
+    height = 71
+
+    # build the blank memory
+    memory = []
+    for r in range(height):
+        for c in range(width):
+            memory.append((r,c))
     
-    return 
+    # remove corrupted memory
+    for c,r in falling_bytes[:1024]:
+        memory.remove((r,c))
+
+    # find the shortest path to the end
+    result = aoc.bfs_shortest_path(memory,(0,0),(70,70))
+    return len(result) - 1
 
 
 
-def part2(pgm, regs):       # => 
+def part2(falling_bytes):       # => 26,22
     """
     Solve part 2
     """
-    return
+    width = 71
+    height = 71
+
+    # build the blank memory
+    memory = []
+    for r in range(height):
+        for c in range(width):
+            memory.append((r,c))
+    
+    # remove corrupted memory
+    for c,r in falling_bytes[:1024]:
+        memory.remove((r,c))
+
+    # we already know there is a valid path through block 1024.
+    # continue dropping blocks and checking the path until there is no solution.
+    for c,r in falling_bytes[1024:]:
+        memory.remove((r,c))
+        if not aoc.bfs_shortest_path(memory,(0,0),(70,70)):
+            return (c,r)
+    
+    return 'path never blocked'
+
 
 
 def solve(puzzle_input):
     """Solve the puzzle for the given input."""
-    p,r = parse(puzzle_input)
+    fb = parse(puzzle_input)
 
     start_time = time.time()
-    p1 = str(part1(p,r))
+    p1 = str(part1(fb))
     exec_time = time.time() - start_time
     print(f"part 1: {p1} ({exec_time:.4f} sec)")
 
     start_time = time.time()
-    p2 = str(part2(p,r))
+    p2 = str(part2(fb))
     exec_time = time.time() - start_time
     print(f"part 2: {p2} ({exec_time:.4f} sec)")
 
