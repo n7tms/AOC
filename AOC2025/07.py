5f1b897517692af1b7db62499c3b0d25a93eb79a
Todd Smith <n7tms@yahoo.com>
2025-12-07 19:14:22 -0700

    2025.07  in progress

---
+++ AOC2025/07.py
@@ -0,0 +1,62 @@
+# AOC 2025 day 07: 
+#
+
+import aoc_utils as aoc
+import time
+import os
+
+DAY = '07'
+IN_FILE = os.path.join("AOC2025","inputs","2025-"+str(DAY)+"-sample.txt")
+# IN_FILE = os.path.join("AOC2025","inputs","2025-"+str(DAY)+".in")
+
+def parse(puzzle_input):
+    """
+    Parse
+    """
+    with open(IN_FILE) as fp:
+        data = fp.read().strip().splitlines()
+    
+    splitters = []
+    for r, line in enumerate(data):
+        for c, ch in enumerate(line):
+            if ch == "S": start = [r,c]
+            elif ch == "^":
+                splitters.append([r,c])
+
+    return start, splitters, [len(data), len(data[0])]
+
+
+def part1(start: list, splitters: list, size: list):        # => 
+    rows, cols = size
+
+    beams = [0] * cols
+    for row in range(1,rows):
+        pass
+
+
+    return 0
+
+
+def part2(x):        # => 
+
+    return 0
+
+
+def solve(puzzle_input):
+    """Solve the puzzle for the given input."""
+    st,sp,si = parse(puzzle_input)
+
+    start_time = time.time()
+    p1 = str(part1(st, sp, si))
+    exec_time = time.time() - start_time
+    print(f"part 1: {p1} ({exec_time:.4f} sec)")
+
+    start_time = time.time()
+    p2 = str(part2(x))
+    exec_time = time.time() - start_time
+    print(f"part 2: {p2} ({exec_time:.4f} sec)")
+
+
+if __name__ == "__main__":
+    solve(IN_FILE)
+        
